ad27f44c5739e3b6537d7e982c153fc5560f35ef
Duccio Rocchini <ducciorocchini@gmail.com>
2025-03-11 17:42:48 +0100

    Create 03_spectral_indices.R

---
+++ Code/03_spectral_indices.R
@@ -0,0 +1,47 @@
+# Code to calculate spectral indices from satellite images
+
+library(imageRy) # beloved package developed at unibo
+library(terra)
+library(viridis)
+
+im.list()
+
+mato1992 = im.import("matogrosso_l5_1992219_lrg.jpg")
+mato1992 = flip(mato1992)
+
+# 1 = NIR
+# 2 = red
+# 3 = green
+
+plot(mato1992)
+im.plotRGB(mato1992, r=1, g=2, b=3)
+im.plotRGB(mato1992, r=2, g=1, b=3)
+im.plotRGB(mato1992, r=2, g=3, b=1)
+
+# Import the 2006 image of the Mato Grosso area
+mato2006 = im.import("matogrosso_ast_2006209_lrg.jpg")
+mato2006 = flip(mato2006)
+plot(mato2006)
+
+im.multiframe(1, 2)
+im.plotRGB(mato1992, r=2, g=3, b=1, title="Mato Grosso 1992")
+im.plotRGB(mato2006, r=2, g=3, b=1, title="Mato Grosso 2006")
+
+# Radiometric resolution
+plot(mato1992[[1]], col=inferno(100))
+plot(mato2006[[1]], col=inferno(100))
+
+# DVI: Difference Vegetation Index
+# E. coli
+# Caribe
+
+# Tree:           NIR=255, red=0, DVI=NIR-red=255-0=255
+# Stressed tree:  NIR=100, red=20, DVI=NIR-red=100-20=80
+
+
+
+
+
+
+
+
