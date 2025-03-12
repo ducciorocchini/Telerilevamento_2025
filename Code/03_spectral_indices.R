a6bd045cc727d12619da980cc7f42439091314d9
Duccio Rocchini <ducciorocchini@gmail.com>
2025-03-12 16:59:20 +0100

    Update 03_spectral_indices.R

--- Code/03_spectral_indices.R
+++ Code/03_spectral_indices.R
@@ -1,10 +1,26 @@
-# Code to calculate spectral indices from satellite images
+# Code for calculating spectral indices in R
 
 library(imageRy) # beloved package developed at unibo
 library(terra)
 library(viridis)
 
 im.list()
+
+mato1992 = im.import("matogrosso_l5_1992219_lrg.jpg")
+mato1992 = flip(mato1992)
+plot(mato1992)
+
+# 1 = NIR
+# 2 = red
+# 3 = green
+
+im.plotRGB(mato1992, r=1, g=2, b=3)
+im.plotRGB(mato1992, r=2, g=1, b=3)
+im.plotRGB(mato1992, r=2, g=3, b=1)
+
+
+#---------------------
+
 
 mato1992 = im.import("matogrosso_l5_1992219_lrg.jpg")
 mato1992 = flip(mato1992)
