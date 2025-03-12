8d3fd8fdd1bf630e1244033cfdbfd466a68a9a35
Duccio Rocchini <ducciorocchini@gmail.com>
2025-03-12 17:48:19 +0100

    Update 03_spectral_indices.R

--- Code/03_spectral_indices.R
+++ Code/03_spectral_indices.R
@@ -18,42 +18,37 @@ im.plotRGB(mato1992, r=1, g=2, b=3)
 im.plotRGB(mato1992, r=2, g=1, b=3)
 im.plotRGB(mato1992, r=2, g=3, b=1)
 
-
-#---------------------
-
-
-mato1992 = im.import("matogrosso_l5_1992219_lrg.jpg")
-mato1992 = flip(mato1992)
-
-# 1 = NIR
-# 2 = red
-# 3 = green
-
-plot(mato1992)
-im.plotRGB(mato1992, r=1, g=2, b=3)
-im.plotRGB(mato1992, r=2, g=1, b=3)
-im.plotRGB(mato1992, r=2, g=3, b=1)
-
-# Import the 2006 image of the Mato Grosso area
 mato2006 = im.import("matogrosso_ast_2006209_lrg.jpg")
 mato2006 = flip(mato2006)
 plot(mato2006)
 
-im.multiframe(1, 2)
-im.plotRGB(mato1992, r=2, g=3, b=1, title="Mato Grosso 1992")
-im.plotRGB(mato2006, r=2, g=3, b=1, title="Mato Grosso 2006")
+im.plotRGB(mato2006, r=1, g=2, b=3)
 
-# Radiometric resolution
-plot(mato1992[[1]], col=inferno(100))
-plot(mato2006[[1]], col=inferno(100))
+im.multiframe(3,2)
+# NIR ontop of red
+im.plotRGB(mato1992, r=1, g=2, b=3)
+im.plotRGB(mato2006, r=1, g=2, b=3)
 
-# DVI: Difference Vegetation Index
+# NIR ontop of green
+im.plotRGB(mato1992, r=2, g=1, b=3)
+im.plotRGB(mato2006, r=2, g=1, b=3)
+
+# NIR ontop of blue
+im.plotRGB(mato1992, r=3, g=2, b=1)
+im.plotRGB(mato2006, r=3, g=2, b=1)
+
+# Exercise: plot only the first layer of mato2006
+dev.off()
+
+plot(mato2006[[1]])
+plot(mato2006[[1]], col=magma(100))
+plot(mato2006[[1]], col=mako(100))
+
+# DVI - Difference Vegetation Index
 # E. coli
 # Caribe
-
-# Tree:           NIR=255, red=0, DVI=NIR-red=255-0=255
-# Stressed tree:  NIR=100, red=20, DVI=NIR-red=100-20=80
-
+# Tree:           NIR=255 (8 bit), red=0 (8 bit), DVI=NIR-red=255-0=255
+# Stressed tree:  NIR=100 (8 bit), red=30 (8 bit), DVI=NIR-red=100-30=70
 
 
 
