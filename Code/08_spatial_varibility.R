477aee1ad62b4b5ce384c0d84473d64b696a8334
Duccio Rocchini <ducciorocchini@gmail.com>
2025-04-10 15:33:06 +0200

    Update 08_spatial_varibility.R

--- Code/08_spatial_varibility.R
+++ Code/08_spatial_varibility.R
@@ -73,5 +73,78 @@ p3 = ggRGB(sent, r=1, g=2, b=3)
 p1 + p2 + p3
 p3 + p1 + p2
 
-# Thursday: stretch, variance, resampling
+# What to do in case of huge images
+
+ncell(sent) * nlyr(sent)
+
+# 794 * 798
+# 2534448
+
+senta = aggregate(sent, fact=2) 
+ncell(senta) * nlyr(senta)
+# 633612
+
+senta5 = aggregate(sent, fact=5)
+ncell(senta5) * nlyr(senta5)
+# 101760
+
+# Exercise: make a multiframe and plot in RGB the three images (or, 2, 5)
+im.multiframe(1,3)
+im.plotRGB(sent, 1, 2, 3)
+im.plotRGB(senta, 1, 2, 3)
+im.plotRGB(senta5, 1, 2, 3)
+
+# Calculating standard deviation
+nira = senta[[1]]
+sd3a = focal(nira, w=c(3,3), fun="sd")
+
+# Exercise: calculate the standard deviation for the factor 5 image
+nira5 = senta5[[1]]
+sd3a5 = focal(nira5, w=c(3,3), fun="sd")
+
+sd5a5 = focal(nira5, w=c(5,5), fun="sd")
+plot(sd5a5)
+
+im.multiframe(1,2)
+plot(sd5a5)
+plot(sd3a)
+
+im.multiframe(2,2)
+plot(sd3)
+plot(sd3a)
+plot(sd3a5)
+plot(sd5a5)
+
+p1 = im.ggplot(sd3)
+p2 = im.ggplot(sd3a)
+p3 = im.ggplot(sd3a5)
+p4 = im.ggplot(sd5a5)
+
+p1 + p2 + p3 + p4
+
+
+im.multiframe(2,2)
+plot(sd3, col=mako(100))
+plot(sd3a, col=mako(100))
+plot(sd3a5, col=mako(100))
+plot(sd5a5, col=mako(100))
+
+# Variance
+# nir 
+var3 = sd3^2
+
+dev.off()
+plot(var3)
+
+im.multiframe(1,2)
+plot(sd3)
+plot(var3)
+
+sd5 = focal(nir, w=c(5,5), fun="sd")
+var5 = sd5^2
+plot(sd5)
+plot(var5)
+
+
+
 # External PDF
